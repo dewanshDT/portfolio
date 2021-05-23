955da98ac18a3459d71552ebe8c2c7e74ba0cd6d
dewanshDT <dewansh.dt@gmail.com>
2021-05-24 00:53:40 +0530

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
